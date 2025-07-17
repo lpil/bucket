5393b1f4507f2c0fd0317c74aed69d33c244facd
Louis Pilfold <louis@lpil.uk>
2025-07-17 11:01:04 +0100

    Update test script

--- test/start-docker-minio.sh
+++ test/start-docker-minio.sh
@@ -3,5 +3,4 @@ docker run -d --rm --name bucket-minio \
   -p 9001:9001 \
   -e "MINIO_ROOT_USER=minioadmin" \
   -e "MINIO_ROOT_PASSWORD=miniopass" \
-  -v ./test/create-buckets.sh:/usr/local/bin/create-buckets.sh \
-  minio/minio server /data
+  lazybit/minio server /data
